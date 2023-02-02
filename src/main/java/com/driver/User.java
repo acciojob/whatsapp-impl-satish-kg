48caf077ad39aa935fa1b528535c435a570c5933
Satish <satishkrgupta555@gmail.com>
2023-02-02 22:20:19 +0530

    testing without set

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -4,9 +4,9 @@ import java.util.HashSet ;
 public class User {
     private String name;
     private String mobile;
-    static HashSet < String > set ;
+//    static HashSet < String > set ;
     public User(String name, String mobile) {
-        new HashSet<>() ;
+//        new HashSet<>() ;
         this.name = name ;
         this.mobile = mobile ;
     }
